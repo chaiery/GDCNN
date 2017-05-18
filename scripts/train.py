95261d1793ed1bd6d512220d070c96366d308d3e
chaiery <yhmyhm@mail.ustc.edu.cn>
2017-05-18 09:29:03 -0400

    test

--- scripts/train.py
+++ scripts/train.py
@@ -18,64 +18,6 @@ max_rotation = 30 #must be intuitive
 max_shift = 20 #must be relevant to total size (1/10 is good)
 
 
-def run_epoch(input_var, label_var, fn, shape):
-    count = int(shape[0]/batch) # number of total batches
-    i = 0
-    err = 0
-    while (i<count):
-        start = i*batch
-        tmp_input = input_var[i*batch:(i+1)*batch]
-        tmp_label = label_var[i*batch:(i+1)*batch]
-
-        e = fn(tmp_input, tmp_label)
-        err += e
-        i += 1 # i is batch number
-
-        pass
-
-    err = err/count #normalize error
-
-    return err
-
-
-def image_transform(input_var, label_var, shape):
-    max_rotation = 30 #must be intuitive
-    max_shift = 20 #must be relevant to total size (1/10 is good)
-    tmp_input = []
-    tmp_label = []
-    for start in range (0, input_var.shape[0]):
-        img=input_var[start,0,:,:]
-        img=img.reshape(shape[2],shape[3])
-        #rotate image
-        r = (random.random()-0.5)*2
-        r_rotate = r*max_rotation # only rotate training images
-        img=scipy.ndimage.interpolation.rotate(img, r_rotate, reshape=False)
-        #shift
-        r = (random.random()-0.5)*2
-        r_shift_x = r*max_shift
-        r = (random.random()-0.5)*2
-        r_shift_y = r*max_shift
-        img = scipy.ndimage.interpolation.shift(img, [r_shift_x, r_shift_y])
-        
-        
-        label = label_var[start,0,:,:]
-        label = label.reshape(shape[2],shape[3])
-        label = scipy.ndimage.interpolation.rotate(label, r_rotate, reshape=False)
-        label = scipy.ndimage.interpolation.shift(label, [r_shift_x, r_shift_y])
-
-        
-        #elastic_transform
-        img, label = elastic.elastic_transform(img, label, 10, 2, random_state=None)
-        
-        img = img.reshape(1,shape[2],shape[3]) #reshape the image to original size afer shift
-        label = label.reshape(1,shape[2],shape[3]) #reshape the image to original size afer shift
-        
-        tmp_input.append(img)
-        tmp_label.append(label)
-    
-    return np.asarray(tmp_input), np.asarray(tmp_label)
-
-
 def run_params(train_input_var, train_label_var, test_input_var, test_label_var):
     start = timeit.default_timer()
     epoch = 1
@@ -135,32 +77,6 @@ def run_params(train_input_var, train_label_var, test_input_var, test_label_var)
     stop = timeit.default_timer()
     print(stop-start)
     print(train_err)
-    '''
-    while (epoch < 101):
-        train_err = run_epoch(train_input_new, train_label_new, train_fn, shape)
-        test_err = run_epoch(test_input_var, test_label_var, test_fn, test_shape)
-        print(test_err,train_err)
-        f.write(str(test_err)+'\t'+str(train_err)+'\n')
-
-        if test_err < best_test_err:
-            best_test_err = test_err
-            best_epoch = epoch
-            best = [np.copy(p) for p in (nn.layers.get_all_param_values(network))]
-            
-        if epoch%50 == 0:
-            params_file='params_epoch_'+str(epoch)
-            with open(params_file, 'wb') as wr:
-                pickle.dump(best, wr)
-                pass
-            wr.close()
-
-        print('%d epoch finished' %(epoch))
-        epoch = epoch+1
-
-        train_input_new, train_label_new = image_transform(train_input_var, train_label_var, shape)
-
-    f.close()
-    '''
 
         
 
